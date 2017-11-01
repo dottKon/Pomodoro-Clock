73ee48f0b58fc003d552d5696ed47daa096d0883
Konrad <konradhyzy@gmail.com>
2017-11-01 14:26:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 //values that can be changed with the settings
-var sec = 5;
+
+
+setWorkSec = 0;
+setWorkMin = 25;
+setBreakSec = 4;
+setBreakMin = 0;
+
+
+//values used in functions
+
 var minutes = 0;
 var seconds = 5;
 var bMinutes = 0;
@@ -14,16 +23,71 @@ var workSeconds = seconds;
 
 
 
+
+//settings
+//click detectors
+var downWork = document.getElementById('downWork');
+if(downWork){
+	downWork.addEventListener("click", workDown);
+}
+function workDown(){
+	if(setWorkMin > 0 || executed){
+		setWorkMin--;
+		minutes = setWorkMin;
+		seconds = setWorkSec;
+		document.getElementById('timer').innerHTML = minutes + ':' + seconds;
+		if(seconds < 10){
+			document.getElementById('timer').innerHTML = minutes + ':' + '0' + seconds;
+		}
+		if(minutes < 10){
+			document.getElementById('timer').innerHTML = '0' +minutes + ':' + seconds;
+		}
+
+		if(seconds < 10 && minutes < 10){
+			document.getElementById('timer').innerHTML = '0' +minutes + ':' + '0' + seconds;
+		}
+		document.getElementById('displayWork').innerHTML = setWorkMin + ':00';
+	}
+}
+
+var upWork = document.getElementById('upWork');
+if(upWork){
+	upWork.addEventListener("click", workUp);
+}
+function workUp(){
+	if(setWorkMin >= 0){
+		setWorkMin++;
+		console.log(setWorkMin);
+		setTime();
+		document.getElementById('displayWork').innerHTML = setWorkMin + ':00';
+	}
+}
+
+
+
+
+
+
+
+
+
 //click detector
 var worker = document.getElementById('roundwork');
-if(worker){
-	worker.addEventListener("click", workTimer);
-};
 
 var breaker = document.getElementById('roundbreak');
-if(breaker){
+
+ 
+if(worker){
+	worker.addEventListener("click", workTimer);
+} else if(breaker){
 	breaker.addEventListener("click", breakTimer);
 };
+
+
+// TODO Alarm sound
+
+
+
 
 //executed detects which phase the timer is in
 var executed = false;
@@ -33,14 +97,22 @@ var breakExecuted = false;
 var intervalWork;
 var intervalBreak;
 
+//checking if the values are the same
+if(workSeconds !== setWorkSec || workMinutes !== setWorkMin){
+	workSeconds = setWorkSec;
+	workMinutes = setWorkMin;
+}	
+
 
 function workTimer(){
 
 	if(!executed){
-		console.log(executed);
 		executed = true;
+		document.getElementById('timer').innerHTML = 'WORK';
+
 
 		intervalWork = setInterval(function(){
+			
 			
 			if(workSeconds == 0){
 				workMinutes--;
@@ -48,23 +120,26 @@ function workTimer(){
 			}
 			workSeconds--;
 			document.getElementById('timer').innerHTML = workMinutes + ':' + workSeconds;
+			document.title = '(' + workMinutes + ':' + workSeconds + ') Work | Pomodoro Timer';
 			if(workSeconds < 10){
 				document.getElementById('timer').innerHTML = workMinutes + ':' + '0' + workSeconds;
+				document.title = "(" + workMinutes + ":" + "0" + workSeconds + ") Work | Pomodoro Timer"
 			}
 			if(workMinutes < 10){
 				document.getElementById('timer').innerHTML = '0' +workMinutes + ':' + workSeconds;
+				document.title = "(0" + workMinutes + ":" + workSeconds + ") Work | Pomodoro Timer"
 			}
 
 			if(workSeconds < 10 && workMinutes < 10){
 				document.getElementById('timer').innerHTML = '0' +workMinutes + ':' + '0' + workSeconds;
+				document.title = "(0" + workMinutes + ":" + "0" + workSeconds + ") Work | Pomodoro Timer"
 			}
 			if(workMinutes == 0 && workSeconds == 0){
-				
+				document.getElementById("roundwork").setAttribute("id", "roundbreak");
 				breakExecuted = false;
 				breakTimer();
 				stopWork();
-				document.getElementById("roundwork").setAttribute("id", "roundbreak");
-				console.log(executed);
+				
 			}
 
 		}, 1000);
@@ -79,17 +154,18 @@ function workTimer(){
 function breakTimer(){
 	if(!breakExecuted){
 		breakExecuted = true;
-		breakMinutes = bMinutes;
-		breakSeconds = bSeconds;
 
+		//making sure we start from the 'settings' time
 
-
+		document.getElementById('timer').innerHTML = 'BREAK';
 		intervalBreak = setInterval(function(){
+			document.title = '(' + breakMinutes + ':' + breakSeconds + ') Break | Pomodoro Timer';
 			if(breakSeconds == 0){
 				breakMinutes--;
 				breakSeconds = 60;
 			}
 			breakSeconds--;
+			document.title = '(' + breakMinutes + ':' + breakSeconds + ') Break | Pomodoro Timer';
 			document.getElementById('timer').innerHTML = breakMinutes + ':' + breakSeconds;
 			if(breakSeconds < 10){
 				document.getElementById('timer').innerHTML = breakMinutes + ':' + '0' + breakSeconds;
@@ -100,16 +176,18 @@ function breakTimer(){
 
 			if(breakSeconds < 10 && breakMinutes < 10){
 				document.getElementById('timer').innerHTML = '0' +breakMinutes + ':' + '0' + breakSeconds;
+				document.title = "(0" + breakMinutes + ":" + "0" + breakSeconds + ") Break | Pomodoro Timer"
 			}
 			if(breakMinutes == 0 && breakSeconds == 0){
 				executed = false;
-				workTimer();
+
 				stopBreak();
+				workTimer();
 				document.getElementById("roundbreak").setAttribute("id", "roundwork");
 			}
 
 		}, 1000); 
-	} else if(!executed){
+	} else if(breakExecuted){
 		clearBreak();
 	}
 }
@@ -121,31 +199,46 @@ function clearWork(){
 	clearInterval(intervalWork);
 	seconds = workSeconds;
 	executed= false;
+	document.getElementById('timer').innerHTML = 'PAUSED';
 }
 
 function clearBreak(){
 	clearInterval(intervalBreak);
+	bSeconds = breakSeconds;
 	breakExecuted = false;
-	workSeconds = sec;
+	document.getElementById('timer').innerHTML = 'PAUSED';
+	
 }
 
 
 
 function stopWork(){
 	clearInterval(intervalWork);
-	seconds = workSeconds;
+	seconds = setWorkSec;
+	minutes = setWorkMin;
+	workSeconds = seconds;
+	workMinutes = minutes;
+	executed = true;
+	worker.removeEventListener('click', workTimer);
+	worker.addEventListener('click', breakTimer);
 }
 
 function stopBreak(){
 	clearInterval(intervalBreak);
-	workSeconds = sec;
+	bSeconds = setBreakSec;
+	bMinutes = setBreakMin;
+	breakSeconds = bSeconds;
+	breakMinutes = bMinutes;
+	worker.removeEventListener('click', breakTimer);
+	worker.addEventListener('click', workTimer);
 }
 
 
 //starter timer on windows load
 function setTime(){
-
-	document.getElementById('timer').innerHTML = minutes + ':' + seconds;
+	minutes = setWorkMin;
+	seconds = setWorkSec
+;	document.getElementById('timer').innerHTML = minutes + ':' + seconds;
 	if(seconds < 10){
 		document.getElementById('timer').innerHTML = minutes + ':' + '0' + seconds;
 	}
